264849c2d487edc752d398e62a755597681ab555
scothook <scot.hook@gmail.com>
2025-09-07 14:52:29 -0700

    adds autofocus on input

--- not-my-diary/src/App.tsx
+++ not-my-diary/src/App.tsx
@@ -51,6 +51,7 @@ function App() {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           onKeyDown={handleKeyPress}
+          autoFocus
         />
       </div>
     </>
