cc66f398014453cf55f0e3fa74e881bfa89c5c47
scothook <scot.hook@gmail.com>
2025-10-12 13:24:52 -0700

    reorder imports

--- not-my-diary/src/App.tsx
+++ not-my-diary/src/App.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
 import Login from './components/Login';
 import CenteredModal from './components/CenteredModal';
 import { useEntries } from './hooks/useEntries';
